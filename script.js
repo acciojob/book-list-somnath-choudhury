e670f6436380623d3e365174dfe6d6971340f28a
Somnath Choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2025-08-17 15:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const submitBtn = document.getElementById("submit");
+const bookList = document.getElementById("book-list");
+
+submitBtn.addEventListener("click", () => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const isbn = document.getElementById("isbn").value;
+
+  if (title ==='' || author === '' || isbn === ''){
+    alert(`Please fill all fields`)
+    return;
+  }
+
+  const row = document.createElement("tr")
+  row.innerHTML = `
+  <td>${title}</td>
+  <td>${author}</td>
+  <td>${isbn}</td>
+  <td><button class = "delete">X</button></td>
+  `
+
+  bookList.appendChild(row);
+
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+})
+
+bookList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.parentElement.remove()
+  }
+})
+
